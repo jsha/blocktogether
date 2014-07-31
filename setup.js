151cf39715dfe7f3e53a621e5b3dfaf210765fab
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-31 00:50:07 -0400

    Try out sequelize

--- setup.js
+++ setup.js
@@ -36,8 +36,40 @@ mysqlConnection.connect(function(err) {
   }
 });
 
+var Sequelize = require('sequelize'),
+    sequelize = new Sequelize('blocktogether', config.dbUser, config.dbPass, {
+      dialect: "mysql",
+      host: config.dbHost,
+      port:    3306,
+    });
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database:', err)
+    } else {
+      console.log('Sequelize onnection has been established successfully.')
+    }
+  })
+
+var Settings = sequelize.define('Settings', {
+  uid: { type: Sequelize.STRING, primaryKey: true },
+  block_egg_mentions: Sequelize.BOOLEAN
+});
+
+sequelize
+  .sync({ force: true })
+    .complete(function(err) {
+       if (!!err) {
+         console.log('An error occurred while creating the table:', err)
+       } else {
+         console.log('It worked!')
+       }
+    })
+
 module.exports = {
   config: config,
   mysqlConnection: mysqlConnection,
-  twitter: twitter
+  twitter: twitter,
+  sequelize: sequelize
 };
