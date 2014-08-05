65a1dec904c5f723fe4c2b2222523ec8299bfb86
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-08-05 12:38:14 -0400

    More robust run-prod.sh

--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-js blocktogether.js >> /tmp/bt.blocktogether.log 2>&1 &
-js stream.js >> /tmp/bt.stream.log 2>&1 &
-js actions.js >> /tmp/bt.actions.log 2>&1 &
-js update-blocks.js >> /tmp/bt.update-blocks.log 2>&1 &
-js update-users.js >> /tmp/bt.update-users.log 2>&1 &
+run() {
+  (while :; do
+     js ${1}.js >> /tmp/bt.${1}.log 2>&1 
+   done) &
+}
+
+run blocktogether
+run stream
+run actions
+run update-blocks
+run update-users
