f4bf3c8c52941ba3f2edd1db387eb32c52fb546a
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-08-09 23:35:00 -0400

    Install packages and restart on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,3 +10,12 @@ set :deploy_to, "/usr/local/blocktogether2"
 # Avoid an error becaues we don't have Rails'
 # public/{images,javascripts,stylesheets} asset structure.
 set :normalize_asset_timestamps, false
+
+after "deploy:create_symlink" do
+  run "cd #{current_path}; npm install -q"
+end
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "killall run-prod.sh js; #{current_path}/run-prod.sh"
+  end
+end
