a29c10d54f53cc539fcf7576a875f7b645c86170
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-08-14 00:23:25 -0400

    Wire it up.

--- static/show-blocks.js
+++ static/show-blocks.js
@@ -1,30 +1,40 @@
 $(function(){
-  function blockAll() {
-    var uids = $('.blocked-user').map(function (el) {
+  function doAction(type) {
+    var checkedUids = $('.checkbox:checked').map(function (el) {
       return $(this).data('uid');
     });
-    $.ajax({
-      type: 'POST',
-      url: '/do-blocks.json',
-      contentType: "application/json",
-      dataType: "json",
-      data: JSON.stringify({
-        list: $.makeArray(uids)
-      }),
-      success: function(data, textStatus, jqXHR) {
-        $('.block-all-processing').show();
-        $('.block-all').hide();
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        alert('Error: ' + textStatus + errorThrown);
-      }
-    });
-  }
-  $('.block-all').click(function(ev) {
-    if ($('.logon').length > 0) {
-      alert('Please log on in order to block people.');
+    if (type == 'block') {
+      $.ajax({
+        type: 'POST',
+        url: '/do-blocks.json',
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({
+          list: $.makeArray(checkedUids)
+        }),
+        success: function(data, textStatus, jqXHR) {
+          $('.block-all-processing').show();
+          $('.block-all').hide();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert('Error: ' + textStatus + errorThrown);
+        }
+      });
     } else {
-      blockAll();
+      alert('action ' + type + ' not yet supported');
+    }
+  }
+  $('button').click(function(ev) {
+    if ($('.logon').length > 0) {
+      alert('Please log on to do that.');
+    } else if ($(ev.target).hasClass('unblock')) {
+      doAction('unblock')
+    } else if ($(ev.target).hasClass('unblock-mute')) {
+      doAction('unblock-mute')
+    } else if ($(ev.target).hasClass('block')) {
+      doAction('unblock-mute')
+    } else {
+      console.log(ev.target);
     }
   });
 });
