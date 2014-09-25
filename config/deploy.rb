68e6922e709612b1e2a331cc0848193c653b6506
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-09-25 15:44:53 -0400

    Add first-time server setup configs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,31 +3,57 @@ set :repository,  "https://github.com/jsha/blocktogether.git"
 
 set :scm, :git
 
-# Server hostname to deploy to.
-role :app, "owb"
-role :staging, "blocktogether-staging"
 set :deploy_to, "/usr/local/blocktogether"
 
 set :deploy_via, :remote_cache
 set :copy_exclude, [ '.git' ]
 
+set :sequelize_config, "/etc/blocktogether/sequelize.json"
+
 # Avoid an error becaues we don't have Rails'
 # public/{images,javascripts,stylesheets} asset structure.
 set :normalize_asset_timestamps, false
 
-after "deploy:create_symlink" do
-  run "cd #{current_path}; npm install -q"
-  run "cd #{current_path}; js ./node_modules/.bin/sequelize --config /etc/blocktogether/sequelize.json -m"
-  run "sudo ln -sf #{current_path}/config/production/upstart/*.conf /etc/init.d/"
-  run "sudo service blocktogether restart"
+# Run cap staging deploy to deploy to staging.
+task :staging do
+  # Hostname blocktogether-staging is an IP alias in jsha's .ssh/config.
+  role :app, *%w[ blocktogether-staging ]
 end
 
-after "deploy:setup" do
-  sudo "chown -R ubuntu.ubuntu #{deploy_to}"
+task :production do
+  role :app, *%w[ blocktogether ]
+end
+
+after "deploy:create_symlink" do
+  run "cd #{current_path}; npm install -q"
+  run "cd #{current_path}; js ./node_modules/.bin/sequelize --config #{sequelize_config} -m"
+  run "sudo ln -sf #{current_path}/config/production/upstart/*.conf /etc/init.d/"
 end
 
 namespace :deploy do
   task :restart do
-    run "killall run-prod.sh js; #{current_path}/run-prod.sh"
+    run "sudo service blocktogether restart"
   end
+end
+
+before "deploy:setup" do
+  dirs = %w{
+          /etc/blocktogether
+          /usr/local/blocktogether
+          /usr/local/blocktogether/releases
+          /data/logs
+          /data/mysql-backup
+        }
+  dirs.each do |dir|
+    sudo "mkdir -p #{dir} -m 0700"
+    sudo "chown ubuntu.ubuntu #{dir}"
+  end
+  upload "config/sequelize.json", "/tmp/sequelize.json", :mode => 0600
+  run "cp -n /tmp/sequelize.json #{sequelize_config}"
+  upload "setup.sh", "/etc/blocktogether/setup.sh", :mode => 0700
+  run "/etc/blocktogether/setup.sh"
+end
+
+after "deploy:setup" do
+  sudo "chown -R ubuntu.ubuntu #{deploy_to}"
 end
--- setup.sh
+++ setup.sh
@@ -1,12 +1,39 @@
-#!/bin/bash
+#!/bin/bash -e
 #
-# Set up a production instance of Block Together
+# Set up a production instance of Block Together. Assumes no existing root MySQL
+# password (as is true of a freshly installed mysql-server).
+# Safe to run multiple times.
 #
+DB_ROOT_PASS=${DB_ROOT_PASS:-$(openssl rand -hex 20)}
+if [ ! -f /usr/sbin/mysqld ] ; then
+  echo "*********************** IMPORTANT ***********************"
+  echo "*********************** IMPORTANT ***********************"
+  echo "This will be your MySQL server root password, write it down:"
+  echo
+  echo $DB_ROOT_PASS
+  echo
+  echo "*********************** IMPORTANT ***********************"
+  echo "*********************** IMPORTANT ***********************"
+  sudo debconf-set-selections <<<'mysql-server mysql-server/root_password password $DB_ROOT_PASS'
+  sudo debconf-set-selections <<<'mysql-server mysql-server/root_password_again password $DB_ROOT_PASS'
+fi
+
+DB_PASS=$(openssl rand -hex 20)
+COOKIE_SECRET=$(openssl rand -hex 20)
 sudo apt-get update
-sudo apt-get install mysql-common mysql-server mysql-client-core-5.5 nodejs nginx
-npm install
-echo "Enter MySQL root password."
-mysql -u root -p <<<EOSQL
-  CREATE DATABASE blocktogether;
-  GRANT ALL ON blocktogether.* to 'blocktogether'@'localhost' IDENTIFIED BY XXX;
+sudo apt-get install -y mysql-client mysql-server nodejs npm git nginx gnupg
+SEQUELIZE_CONFIG=/etc/blocktogether/sequelize.json
+if grep -q __PASSWORD__ $SEQUELIZE_CONFIG ; then
+  sed -i s/__PASSWORD__/$DB_PASS/g $SEQUELIZE_CONFIG
+  mysql -u root --password="$DB_ROOT_PASS" <<EOSQL
+    CREATE DATABASE IF NOT EXISTS blocktogether;
+    GRANT ALL ON blocktogether.* to 'blocktogether'@'localhost' IDENTIFIED BY "${DB_PASS}";
 EOSQL
+fi
+if ! crontab -l >/dev/null; then
+  crontab <<EOCRON
+    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+    MAILTO=ubuntu
+    23 10 * * * bash /usr/local/blocktogether/current/util/backup-db.sh
+EOCRON
+fi
