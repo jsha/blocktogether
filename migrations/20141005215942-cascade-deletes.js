6c94c4f828c262193ac34a052fe5dead977a4b02
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-10-15 17:51:48 -0400

    Cascade updates as well as deletes.

--- migrations/20141005215942-cascade-deletes.js
+++ migrations/20141005215942-cascade-deletes.js
@@ -3,15 +3,21 @@ module.exports = {
     // Blocks get deleted when their BlockBatch gets deleted.
     migration.queryInterface.sequelize.query(
       'ALTER TABLE Blocks ADD ' +
-      'CONSTRAINT `Blocks_ibfk_1` FOREIGN KEY (`BlockBatchId`) REFERENCES `BlockBatches` (`id`) ON DELETE CASCADE;');
+      'CONSTRAINT `Blocks_ibfk_1` FOREIGN KEY (`BlockBatchId`) ' +
+      'REFERENCES `BlockBatches` (`id`) ' +
+      'ON DELETE CASCADE ON UPDATE CASCADE;');
     // BlockBatches get deleted when their BtUser gets deleted.
     migration.queryInterface.sequelize.query(
       'ALTER TABLE BlockBatches ADD ' +
-      'CONSTRAINT `BlockBatches_ibfk_1` FOREIGN KEY (`source_uid`) REFERENCES `BtUsers` (`uid`) ON DELETE CASCADE;');
+      'CONSTRAINT `BlockBatches_ibfk_1` FOREIGN KEY (`source_uid`) ' +
+      'REFERENCES `BtUsers` (`uid`) ' +
+      'ON DELETE CASCADE ON UPDATE CASCADE;');
     // Actions get deleted when their source BtUser gets deleted.
     migration.queryInterface.sequelize.query(
       'ALTER TABLE Actions ADD ' +
-      'CONSTRAINT `Actions_ibfk_1` FOREIGN KEY (`source_uid`) REFERENCES `BtUsers` (`uid`) ON DELETE CASCADE;');
+      'CONSTRAINT `Actions_ibfk_1` FOREIGN KEY (`source_uid`) ' +
+      'REFERENCES `BtUsers` (`uid`) ' +
+      'ON DELETE CASCADE ON UPDATE CASCADE;');
     done()
   },
   down: function(migration, DataTypes, done) {
