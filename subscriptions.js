13f7e6ffb4305c2d3add9e312e91950c218b7f3c
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-11-26 14:07:13 -0800

    For subscriptions, calculate should-be-blocked and should-be-unblocked lists.

--- subscriptions.js
+++ subscriptions.js
@@ -6,6 +6,7 @@ var Promise = require('q'),
 
 var logger = setup.logger,
     Action = setup.Action,
+    Block = setup.Block,
     Subscription = setup.Subscription;
 
 /**
@@ -121,6 +122,131 @@ function unblockFromSubscription(proposedUnblock) {
     logger.error(err);
     return Q.resolve(null);
   });
+}
+
+/**
+ * Get a list of all the blocks for uid from their latest complete BlockBatch.
+ * @return {Promise.<Array.<Block> >} a list of blocks.
+ */
+function getLatestBlocks(uid) {
+  return BlockBatch.find({
+    where: {
+      source_uid: uid,
+      complete: true
+    },
+    order: 'updatedAt desc'
+  }).then(function(blockBatch) {
+    if (blockBatch) {
+      return Block.findAll({
+        where: {
+          blockBatchId: blockBatch.id
+        }
+      });
+    } else {
+      return Q.reject('No blockBatch available for', user);
+    }
+  });
+}
+
+/**
+ * For a given uid, return an Object whose keys are the uids of all accounts
+ * that user has ever unblocked (auto-unblocks from subscriptions do not
+ * count). The values of the object are single Actions demonstrating the
+ * unblock. If the user unblocked an account multiple times, only one Action is
+ * included, and it is not guaranteed to be the latest.
+ * @param {string} uid User id whose Actions to look at.
+ * @return {Promise.<Object>} Object mapping uid -> unblock Actions.
+ */
+function getManualUnblocks(uid) {
+  return Actions.find({
+    where: {
+      type: Action.UNBLOCK,
+      source_uid: uid,
+      cause: {
+        not: [Action.SUBSCRIPTION]
+      }
+    }
+  }).then(function(actions) {
+    return _.indexBy(actions, 'sink_uid');
+  });
+}
+
+/**
+ * For a given user, find all their subscriptions, and all the accounts
+ * included on those subscription block lists. Return an object mapping from
+ * blocked account uid -> list of authors blocking that user.
+ * @param {BtUser} user
+ * @return {Object}
+ */
+function subscriptionBlocksAuthors(user) {
+  var subscriptionsPromise = Subscription.findAll({
+    where: {
+      subscriber_uid: user.uid
+    }
+  }).then(function(subscriptions) {
+    if (subscriptions && subscriptions.length > 0) {
+      var authors = _.pluck(subscriptions, 'author_uid');
+      return Q.all(authors.map(getLatestBlocks))
+        .then(function(blocklists) {
+        var authorsBlocklists = _.zip(authors, blocklists);
+        // Create a mapping from a sink_uid (i.e. a blocked account) to a list
+        // of subscribed author_uids who have that sink_uid blocked.
+        var blocksAuthors = {};
+        blocksAuthors.forEach(function(pair) {
+          var author_uid = pair[0];
+          var blocklist = pair[0];
+          blocklist.forEach(function(block) {
+            var sink_uid = block.sink_uid;
+            if (!blocksAuthors[sink_uid]) {
+              blocksAuthors[sink_uid] = [author_uid];
+            } else {
+              blocksAuthors[sink_uid].push(author_uid);
+            }
+          });
+        });
+        return blocksAuthors;
+      });
+    } else {
+      logger.debug('No subscriptions for', user);
+      return {};
+    }
+  });
+}
+
+/**
+ * Given an object, delete all keys listed in array.
+ * @param {Object} obj Object to be deleted from.
+ * @param {Array} array Array whose entries should be used as keys to delete.
+ */
+function deleteFromObject(obj, array) {
+  array.forEach(function(item) {
+    delete obj[item];
+  });
+}
+
+function fixUp(user) {
+  var subscriptionBlocksAuthorsPromise = subscriptionBlocksAuthors(user);
+  var blocksPromise = getLatestBlocks(user.uid);
+  var unblocksPromise = getManualUnblocks(user.uid);
+
+  Q.spread([subscriptionBlocksAuthorsPromise, blocksPromise, unblocksPromise],
+    function(blocksAuthors, blocks, unblocks) {
+      var toBeBlocked = _.clone(blocksAuthors);
+      deleteFromObject(toBeBlocked, _.pluck(blocks, 'sink_uid'));
+      deleteFromObject(toBeBlocked, Object.keys(unblocks));
+      var toBeBlockedUids = Object.keys(toBeBlocked);
+      // TODO: Actually enqueue blocks for these users.
+      logger.info('User', user, 'should block', toBeBlockedUids.length,
+        'accounts for subscriptions:', toBeBlockedUids);
+
+      var toBeUnblocked = _.indexBy(blocks, 'sink_uid');
+      deleteFromObject(toBeUnblocked, Object.keys(blocksAuthors));
+      // TODO: From the remaining blocks, delete any that don't have cause =
+      // Subscription and cause_uid = [one of currently subscribed authors].
+      // This will be easiest to do with Annotated Blocks.
+    }).catch(function(err) {
+      logger.error(err);
+    });
 }
 
 module.exports = {
