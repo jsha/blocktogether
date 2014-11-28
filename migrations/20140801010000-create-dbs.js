fb41972a0fc9c640ec0914cebe1d70b063eccb4b
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-11-28 18:26:15 -0500

    Fix asynchronous table creation migration

--- migrations/20140801010000-create-dbs.js
+++ migrations/20140801010000-create-dbs.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
-    migration.createTable(
+    return migration.createTable(
       'TwitterUsers', {
       uid: {
         type: 'VARCHAR(20)',
@@ -16,88 +16,92 @@ module.exports = {
     }, {
       charset: 'utf8mb4',
       collate: 'utf8mb4_unicode_ci'
-    });
-    migration.createTable(
-      'BtUsers', {
-      uid: {
-        type: 'VARCHAR(20)',
-        primaryKey: true
-      },
-      screen_name: DataTypes.STRING,
-      access_token: DataTypes.STRING,
-      access_token_secret: DataTypes.STRING,
-      shared_blocks_key: DataTypes.STRING,
-      block_new_accounts: DataTypes.BOOLEAN,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    }, {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_unicode_ci'
-    });
-    migration.createTable('BlockBatches', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      source_uid: {
-        type: 'VARCHAR(20)',
-        primaryKey: true
-      },
-      currentCursor: DataTypes.STRING,
-      complete: DataTypes.BOOLEAN,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    }, {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_unicode_ci'
-    });
-    migration.createTable('Blocks', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      sink_uid: DataTypes.STRING,
-      type: DataTypes.STRING,
-      BlockBatchId: DataTypes.INTEGER,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    }, {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_unicode_ci'
-    });
-    migration.createTable('Actions', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      source_uid: 'VARCHAR(20)',
-      sink_uid: 'VARCHAR(20)',
-      type: DataTypes.STRING,
-      status: DataTypes.STRING,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    }, {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_unicode_ci'
-    });
-    migration.createTable('UnblockedUsers', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      source_uid: 'VARCHAR(20)',
-      sink_uid: 'VARCHAR(20)',
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE
-    }, {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_unicode_ci'
-    });
-    done();
+    }).then(function() {
+      return migration.createTable(
+        'BtUsers', {
+        uid: {
+          type: 'VARCHAR(20)',
+          primaryKey: true
+        },
+        screen_name: DataTypes.STRING,
+        access_token: DataTypes.STRING,
+        access_token_secret: DataTypes.STRING,
+        shared_blocks_key: DataTypes.STRING,
+        block_new_accounts: DataTypes.BOOLEAN,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+      }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      });
+    }).then(function() {
+      return migration.createTable('BlockBatches', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        source_uid: {
+          type: 'VARCHAR(20)',
+          primaryKey: true
+        },
+        currentCursor: DataTypes.STRING,
+        complete: DataTypes.BOOLEAN,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+      }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      });
+    }).then(function() {
+      return migration.createTable('Blocks', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        sink_uid: DataTypes.STRING,
+        type: DataTypes.STRING,
+        BlockBatchId: DataTypes.INTEGER,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+      }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      });
+    }).then(function() {
+      return migration.createTable('Actions', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        source_uid: 'VARCHAR(20)',
+        sink_uid: 'VARCHAR(20)',
+        type: DataTypes.STRING,
+        status: DataTypes.STRING,
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+      }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      });
+    }).then(function() {
+      return migration.createTable('UnblockedUsers', {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        source_uid: 'VARCHAR(20)',
+        sink_uid: 'VARCHAR(20)',
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE
+      }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      });
+    }).then(done);
   },
   down: function(migration, DataTypes, done) {
     done();
