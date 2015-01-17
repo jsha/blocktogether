2dcb8982dcfc60cf3be6da9e5ecfac9ce7a70c09
Sara Bee <sara.bainbridge@gmail.com>
2015-01-17 13:07:58 -0800

    Add some stuff to gitignore; add missing config to development.json; further tweak Vagrant up script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,14 @@
 # #!/bin/bash
 sudo rm -rf /etc/blocktogether
 sudo mkdir /etc/blocktogether
-sudo chown -R vagrant /etc/blocktogether
 cp -r /vagrant/config/* /etc/blocktogether/
 mv /etc/blocktogether/development.json /etc/blocktogether/config.json
 
 cd /vagrant
 source ./setup.sh
 su - vagrant -c'source /vagrant/install_nvm.sh'
+
+sudo chown -R vagrant /etc/blocktogether
 
 echo "*********************** IMPORTANT ***********************"
 echo ""
@@ -16,8 +17,9 @@ echo "Get your consumerKey and consumerSecret and add them to the config file:"
 echo ""
 echo "     vagrant ssh && sudo vim /etc/blocktogether/config.json"
 echo ""
-echo "Afterwards, you can start the server with:"
+echo "Afterwards, you can start the server from the VM with:"
 echo ""
 echo "     vagrant ssh && ./run.sh"
 echo ""
+echo "It can then be accessed "
 echo "*********************** IMPORTANT ***********************"
