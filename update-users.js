1b0cde730088fab32a2631985991d81000537542
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-05-22 15:59:08 -0700

    Improve update-users
    Use multiple credentials from the DB to enable more frequent updates.
    Export the updateUsers function and make it use Promises.
    updateUsers can now handle input of any length and turn it into multiple
    queries.

--- update-users.js
+++ update-users.js
@@ -1,16 +1,15 @@
 'use strict';
 (function() {
 /** @type{SetupModule} */
-var setup = require('./setup');
-var _ = require('sequelize').Utils._;
-var verifyCredentials = require('./verify-credentials');
+var setup = require('./setup'),
+    Q = require('q'),
+    _ = require('sequelize').Utils._,
+    verifyCredentials = require('./verify-credentials');
 
 var config = setup.config,
     twitter = setup.twitter,
     logger = setup.logger,
     sequelize = setup.sequelize,
-    accessToken = config.defaultAccessToken,
-    accessTokenSecret = config.defaultAccessTokenSecret,
     BtUser = setup.BtUser,
     TwitterUser = setup.TwitterUser,
     BlockBatch = setup.BlockBatch,
@@ -20,6 +19,10 @@ var config = setup.config,
  * Find TwitterUsers needing update, look them up on Twitter, and store in
  * database. A user needs update if it's just been inserted (no screen name)
  * or if it hasn't been updated in a day.
+ *
+ * We fetch a relatively large chunk from the DB every few seconds, rather than
+ * smaller chunks every hundred milliseconds, to ensure there's enough time
+ * between each DB query for the API query to complete and to write to the DB.
  *
  * @param {string} sqlFilter An SQL `where' clause to filter users by. Allows
  *   running separate update cycles for fresh users (with no screen name) vs
@@ -31,13 +34,11 @@ function findAndUpdateUsers(sqlFilter) {
       where: sequelize.and(
         { deactivatedAt: null },
         sqlFilter),
-      limit: 100
-    }).error(function(err) {
+      limit: 5000
+    }).then(function(users) {
+      updateUsers(_.pluck(users, 'uid'));
+    }).catch(function(err) {
       logger.error(err);
-    }).success(function(users) {
-      if (users.length > 0) {
-        updateUsers(_.pluck(users, 'uid'));
-      }
     });
 }
 
@@ -101,32 +102,69 @@ function deactivateTwitterUser(uid) {
   });
 }
 
+var userCredentials = [];
+var userCredentialsIndex = 0;
 
 /**
  * Given a list of uids, look them up using the Twitter API and update the
- * database accordingly.
+ * database accordingly. Split up into chunks of 100, the max the API will
+ * return.
+ *
  * @param {Array.<string>} uids List of user ids to look up.
+ * @return{Promise.<Object>} map of uids succesfully returned to user objects.
  */
 function updateUsers(uids) {
-  twitter.users('lookup', {
-      skip_status: 1,
-      user_id: uids.join(',')
-    },
-    accessToken, accessTokenSecret,
-    updateUsersCallback.bind(null, uids));
+  var chunkedUids = [];
+  while (uids.length > 0) {
+    chunkedUids.push(uids.splice(0, 100));
+  }
+  return Q.all(
+    chunkedUids.map(updateUsersChunk)
+  ).then(function(results) {
+    var ret = _.reduce(results, _.assign, {});
+    console.log(Object.keys(ret));
+    return ret;
+  })
 }
 
 /**
- * Given a user lookup API response from Twitter, store the user into the DB.
- * @param {Array.<string>} uids Array of uids that were requested.
- * @param {Object} err Error return by Twitter API, if any.
- * @param {Array.<Object>} response List of JSON User objects as defined by the
- *   Twitter API. https://dev.twitter.com/docs/platform-objects/users
+ * Given a list of less than 100 uids, look them up using the Twitter API and
+ * update the database accordingly.
+ * @param {Array.<string>} uids List of user ids to look up.
+ * @return{Object} map of uids succesfully returned to user objects.
  */
-function updateUsersCallback(uids, err, response) {
-  if (err) {
+function updateUsersChunk(uids) {
+  if (!userCredentials.length) {
+    throw "user credentials not loaded";
+  }
+  // Iterate through the user credentials to spread out rate limit usage.
+  var credential = userCredentials[userCredentialsIndex];
+  userCredentialsIndex = (userCredentialsIndex + 1) % userCredentials.length;
+  return Q.ninvoke(twitter, 'users', 'lookup', {
+    skip_status: 1,
+    user_id: uids.join(',')
+  },
+  credential.access_token, credential.access_token_secret
+  ).spread(function(response, httpResponse) {
+    logger.debug('Got /users/lookup response size', response.length,
+      'for', uids.length, 'uids');
+
+    // When a user is suspended, deactivated, or deleted, Twitter will simply not
+    // return that user object in the response. Delete those users so they don't
+    // clog future lookup attempts.
+    var indexedResponses = _.indexBy(response, 'id_str');
+    uids.forEach(function(uid) {
+      if (indexedResponses[uid]) {
+        storeUser(indexedResponses[uid]);
+      } else {
+        logger.info('TwitterUser', uid, 'suspended, deactivated, or deleted. Marking so.');
+        deactivateTwitterUser(uid);
+      }
+    });
+    return indexedResponses;
+  }).catch(function(err) {
     if (err.statusCode === 429) {
-      logger.info('Rate limited.');
+      logger.info('Rate limited /users/lookup.');
     } else if (err.statusCode === 404) {
       // When none of the users in a lookup are available (i.e. they are all
       // suspended or deleted), Twitter returns 404. Delete all of them.
@@ -137,21 +175,6 @@ function updateUsersCallback(uids, err, response) {
       logger.error('Error /users/lookup', err.statusCode, err.data);
     }
     return;
-  }
-  logger.debug('Got /users/lookup response size', response.length,
-    'for', uids.length, 'uids');
-
-  // When a user is suspended, deactivated, or deleted, Twitter will simply not
-  // return that user object in the response. Delete those users so they don't
-  // clog future lookup attempts.
-  var indexedResponses = _.indexBy(response, 'id_str');
-  uids.forEach(function(uid) {
-    if (indexedResponses[uid]) {
-      storeUser(indexedResponses[uid]);
-    } else {
-      logger.info('TwitterUser', uid, 'suspended, deactivated, or deleted. Marking so.');
-      deactivateTwitterUser(uid);
-    }
   });
 }
 
@@ -203,19 +226,28 @@ function storeUser(twitterUserResponse) {
 }
 
 module.exports = {
-  findAndUpdateUsers: findAndUpdateUsers,
+  updateUsers: updateUsers,
   storeUser: storeUser
 };
 
 if (require.main === module) {
-  findAndUpdateUsers();
-  // Poll for just-added users every 1 second and do an initial fetch of their
-  // information.
-  setInterval(findAndUpdateUsers.bind(null, 'screen_name IS NULL'), 5000);
-  // Poll for users needing update every 10 seconds.
-  setInterval(
-    findAndUpdateUsers.bind(null, 'updatedAt < (now() - INTERVAL 1 DAY)'), 10000);
-  // Every ten seconds, check credentials of some subset of users.
-  setInterval(verifyMany, 10000);
+  BtUser.findAll({
+    where: {
+      deactivatedAt: null
+    },
+    limit: 100
+  }).then(function(users) {
+    userCredentials = users;
+    findAndUpdateUsers();
+    // Poll for just-added users every 1 second and do an initial fetch of their
+    // information.
+    setInterval(findAndUpdateUsers.bind(null, 'screen_name IS NULL'), 5000);
+    // Poll for users needing update every 10 seconds.
+    setInterval(
+      findAndUpdateUsers.bind(null, 'updatedAt < (now() - INTERVAL 1 DAY)'), 5000);
+    // Every ten seconds, check credentials of some subset of users.
+    setInterval(verifyMany, 10000);
+  });
 }
+
 })();
