971ec0586d956ccf57ea4171e8c18e4b7e399c0c
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-07-25 12:04:05 -0700

    Follow streams for all users.

--- stream.js
+++ stream.js
@@ -112,13 +112,7 @@ function refreshUsers() {
           gt: allUsersLastUpdate
         },
       },
-      ['uid % ? = ?', numWorkers, workerId % numWorkers],
-      // Check for any option that monitors stream for autoblock criteria
-      sequelize.or(
-        { block_new_accounts: true },
-        { block_low_followers: true },
-        { shared_blocks_key: { not: null } }
-      ))
+      ['uid % ? = ?', numWorkers, workerId % numWorkers])
     }).then(function(users) {
       _.extend(allUsers, _.indexBy(users, 'uid'));
       allUsersLastUpdate = now;
