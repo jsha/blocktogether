cb8942fe1d304d248808000ab99e22a570d655e1
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-01-10 12:18:29 -0800

    Migration to Actions2.

--- migrations/20160102210800-modify-statuses.js
+++ migrations/20160102210800-modify-statuses.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-      return queryInterface.addColumn('Actions',
-        'statusNum', 'TINYINT'
-      ).then(function() {
-        return queryInterface.addColumn('Actions',
-          'causeNum', 'TINYINT');
-      }).then(function() {
-        return queryInterface.addColumn('Actions',
-          'typeNum', 'TINYINT');
-      }).then(function() {
-        var query = 'UPDATE Actions SET ' +
-          'statusNum = FIELD(status, "pending", "done", "cancelled-following", "cancelled-suspended", "cancelled-duplicate", "cancelled-unblocked", "cancelled-self", "deferred-target-suspended", "cancelled-source-deactivated", "cancelled-unsubscribed"), ' + 
-          'causeNum = FIELD(cause, "external", "subscription", "new-account", "low-followers", "bulk-manual-block"), ' +
-          'typeNum = FIELD(type, "block", "unblock", "mute");'
-        return queryInterface.sequelize.query(query);
+    var create =
+      'CREATE TABLE `Actions2` (' +
+      '`id` int(11) NOT NULL AUTO_INCREMENT,' +
+      '`source_uid` BIGINT UNSIGNED NOT NULL,' +
+      '`sink_uid` BIGINT UNSIGNED NOT NULL,' +
+      '`type` TINYINT(1),' +
+      '`status` TINYINT(1),' +
+      '`createdAt` datetime NOT NULL,' +
+      '`updatedAt` datetime NOT NULL,' +
+      '`cause` TINYINT(1) NOT NULL,' +
+      '`cause_uid` BIGINT UNSIGNED NOT NULL,' +
+      'PRIMARY KEY (`id`),' +
+      'KEY `actions_source_uid_sink_uid` (`source_uid`,`sink_uid`),' +
+      'KEY `actions_source_uid_status_created_at` (`source_uid`,`status`,`createdAt`)' +
+      ') ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;';
+
+    var insert =
+      'INSERT INTO Actions2 SELECT ' +
+      'id, source_uid, sink_uid, ' +
+      'FIELD(type, "block", "unblock", "mute"), ' +
+      'FIELD(status, "pending", "done", "cancelled-following", "cancelled-suspended", "cancelled-duplicate", "cancelled-unblocked", "cancelled-self", "deferred-target-suspended", "cancelled-source-deactivated", "cancelled-unsubscribed"), ' +
+      'createdAt, updatedAt, ' +
+      'FIELD(cause, "external", "subscription", "new-account", "low-followers", "bulk-manual-block"), ' +
+      'cause_uid FROM Actions;';
+
+      return queryInterface.sequelize.query(create).then(function() {
+        return queryInterface.sequelize.query(insert);
       });
   },
 
