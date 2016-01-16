f15af63ee3e35138a9992e0c38f6dd607310cb99
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-01-16 11:10:29 -0800

    Move to Node 5.x.

--- bin/setup.sh
+++ bin/setup.sh
@@ -28,8 +28,8 @@ DB_PASS=$(openssl rand -hex 20)
 # Set up the nodesource Node repo to get the latest.
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 tee /etc/apt/sources.list.d/nodesource.list <<EOAPT
-deb https://deb.nodesource.com/node_0.12 trusty main
-deb-src https://deb.nodesource.com/node_0.12 trusty main
+deb https://deb.nodesource.com/node_5.x trusty main
+deb-src https://deb.nodesource.com/node_5.x trusty main
 EOAPT
 
 apt-get update
