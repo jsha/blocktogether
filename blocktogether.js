565158ffd40dfccbe6f8a94cd2fef7cbffb203d2
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-06-14 20:13:13 -0700

    Increase max subscribe size.

--- blocktogether.js
+++ blocktogether.js
@@ -29,7 +29,7 @@ var config = setup.config,
     Subscription = setup.Subscription;
 
 // Maximum size of a block list that can be subscribed to.
-const maxSubscribeSize = 150000;
+const maxSubscribeSize = 250000;
 
 // Look for templates here
 mu.root = __dirname + '/templates';
